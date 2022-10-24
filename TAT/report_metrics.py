744535efd01e365810bf3179fc53cf29f6a3e6cd
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2022-10-23 20:56:46 -0400

    Update metrics

--- TAT/report_metrics.py
+++ TAT/report_metrics.py
@@ -7,13 +7,28 @@ model_names = ["GCN", "GAT", "GraphSage", "TAGCN", "DE-GNN", "TAT"]
 datasets = ["SMS-A", "CollegeMsg"]
 
 for i, model in enumerate(models):
+
     print(model_names[i], end=' ')
+
     for dataset in datasets:
         res = pd.read_csv(f"./TAT/Results/{dataset}/{model}.csv")
+
         acc_idx = np.argmax(res["val_acc"])
         auc_idx = np.argmax(res["val_auc"])
         test_acc = res["test_acc"][acc_idx]
         test_auc = res["test_auc"][auc_idx]
 
-        print(f"& {np.round(test_acc, 3): .3f} & {np.round(test_auc, 3): .3f}", end=' ')
+        s = f"& {np.round(test_acc, 3): .3f} & {np.round(test_auc, 3): .3f} "
+
+        if "test_bleu" in res.columns:
+            idx = np.argmax(res["val_bleu"])
+            test_val = res["test_bleu"][idx]
+            s += f"& {np.round(test_val, 3): .3f}"
+
+        if "test_kendall_tau" in res.columns:
+            idx = np.argmax(res["val_kendall_tau"])
+            test_val = res["test_kendall_tau"][idx]
+            s += f"& {np.round(test_val, 3): .3f}"
+
+        print(s, end=' ')
     print("\\\\")
